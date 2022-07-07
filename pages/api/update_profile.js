df17014fe3ea649cc606d138bb49db1e214419e8
Pasubathi <73518189+Pasubathi@users.noreply.github.com>
2022-07-07 13:07:29 +0530

    Update update_profile.js

--- pages/api/update_profile.js
+++ pages/api/update_profile.js
@@ -17,10 +17,8 @@ export default async function updateProfile(req, res) {
                 return res.status(500).end(`Method ${req.method} Not Allowed`)
         }
         async function updateProfile() {
-            const { user_id, gst_no, company_name, address, pincode, email, gst_composite, mobile, nature_business, entity_type, cin_no, pan_no, website, licence_title, licence_number  } = req.body;
+            const { user_id, gst_no, company_name, address, pincode, gst_composite, mobile, nature_business, entity_type, cin_no, pan_no, website, licence_title, licence_number  } = req.body;
             if (user_id == "" || user_id == undefined)
-                return res.status(500).send({ message: "User id is mandatory" })
-            if (email == "" || email == undefined)
                 return res.status(500).send({ message: "Email is mandatory" })
             if (mobile == "" || mobile == undefined)
                 return res.status(500).send({ message: "Mobile is mandatory" })
@@ -33,39 +31,28 @@ export default async function updateProfile(req, res) {
                 if (!user) {
                     return res.status(200).send({ status:'error',  message: "Invalid account" })
                 } else {
-                    if(user.password__c == current_password)
-                   {
-                        if(password == cpassword)
-                        {
-                            await prisma.account__c.update({
-                                data:{
-                                    phone__c: mobile,
-                                    email__c: email,
-                                    gstin_cc: gst_no?gst_no:null,
-                                    compant_name__c: company_name?company_name:null,
-                                    address__c: address?address:null,
-                                    pincode__c: pincode?pincode:null,
-                                    gst_composite__c: gst_composite?gst_composite:null,
-                                    business_nature__c: nature_business?nature_business:null,
-                                    entity_type__c: entity_type?entity_type:null,
-                                    cin__c: cin_no?cin_no:null,
-                                    pan__c: pan_no?pan_no:null,
-                                    website__c: website?website:null,
-                                    license_title__c: licence_title?licence_title:null,
-                                    license_number__c: licence_number?licence_number:null
-                                },
-                                where: {
-                                    id: Number(user_id)
-                                }
-                            });
-                             return res.status(200).send({ status:'success',  message: "Password Updated Successfully"})
-                        }else{
-                            return res.status(200).send({ status:'error',  message: "Password mismatch" })
+                    await prisma.account__c.update({
+                        data:{
+                            phone__c: mobile,
+                            gstin_cc: gst_no?gst_no:null,
+                            compant_name__c: company_name?company_name:null,
+                            address__c: address?address:null,
+                            pincode__c: pincode?pincode:null,
+                            gst_composite__c: gst_composite?gst_composite:null,
+                            business_nature__c: nature_business?nature_business:null,
+                            entity_type__c: entity_type?entity_type:null,
+                            cin__c: cin_no?cin_no:null,
+                            pan__c: pan_no?pan_no:null,
+                            website__c: website?website:null,
+                            license_title__c: licence_title?licence_title:null,
+                            license_number__c: licence_number?licence_number:null
+                        },
+                        where: {
+                            id: Number(user_id)
                         }
-                    }else{
-                        return res.status(200).send({ status:'error',  message: "Current password does't match" })
-                    }
-                }
+                    });
+                        return res.status(200).send({ status:'success',  message: "Updated Successfully"})
+        }
             } catch (e) {
                 res.status(500).send({ message: e.message ? e.message : e });
                 return;
