fb937881e98f1005c07c2a4d9b6b0f5637d4997d
murugan-pandurangan <p.murugan103@gmail.com>
2022-07-16 13:23:14 +0530

    updates

--- pages/api/get_purchase_by_id.js
+++ pages/api/get_purchase_by_id.js
@@ -7,57 +7,62 @@ const cors = initMiddleware(
       methods: ['GET', 'POST', 'OPTIONS'],
     })
 )
-export default async function getDebitById(req, res) {
+export default async function getPurchaseById(req, res) {
     await cors(req, res);
     try {
         switch (req.method) {
             case 'POST':
-                return getDebitById();
+                return getPurchaseById();
             default:
                 return res.status(500).end(`Method ${req.method} Not Allowed`)
         }
-        async function getDebitById() {
-            const { debit_id } = req.body;
-            if (debit_id == "" || debit_id == undefined)
-                return res.status(200).send({ status:'error', message: "Debit note id is mandatory" })
+        async function getPurchaseById() {
+            const { purchase_id } = req.body;
+            if (purchase_id == "" || purchase_id == undefined)
+                return res.status(200).send({ status:'error', message: "Purchase id is mandatory" })
             try {
-                const debit = await prisma.debit_note__c.findUnique({
+                const purchase = await prisma.purchase__c.findUnique({
                     select:{
                         id: true,
                         account_id: true,
-                        debit_note_no__c: true,
                         purchase_invoice_no__c: true,
                         vendor_gstin__c: true,
                         vendor_name__c: true,
                         vendor_address__c: true,
                         vendor_mob_no__c: true,
-                        purchase_date__c: true,
+                        delivery_date__c: true,
                         date__c: true,
+                        delivery_address__c: true,
+                        delivery_to__c: true,
                         place_of_supply__c: true,
                         tax_type__c: true,
                         subtotal__c: true,
                         cgst__c: true,
                         sgst__c: true,
+                        amount_paid__c: true,
                         total_amount__c: true,
-                        sale_preference__c: true,
+                        balance_due__c: true, 
+                        shipment_preference__c: true,
+                        shipping_charges__c: true,
                         purchase_note__c: true,
                         status__c: true
                     },
                     where: {
-                        id: Number(debit_id)
+                        id: Number(purchase_id)
                     }
                 });
-                if (!debit) {
+
+                if (!purchase) {
                     return res.status(200).send({ status:'error',  message: "Invalid Debit" })
                 } else {
-                    let debit_data = debit;
-                   const products = await prisma.debit_order__c.findMany({
+                   let purchase_data = purchase;
+                   const products = await prisma.purchase_order__c.findMany({
                     where: {
-                        debit_note_id__c: Number(debit.id)
+                        purchase_id__c: Number(purchase.id)
                     }
                     });
-                    debit_data.products = products;
-                    return res.status(200).send({ status:'success',  message: "Success", data: debit_data})
+                    purchase_data.products = products;
+                    return res.status(200).send({ status:'success',  message: "Success", data: purchase_data})
                 }
             } catch (e) {
                 res.status(200).send({ status:'error', message: e.message ? e.message : e });
