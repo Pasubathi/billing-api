a1ef96e6e50446f78ece42540cfe4468314021a4
murugan-pandurangan <p.murugan103@gmail.com>
2022-07-22 12:10:19 +0530

    Create delete_vendor.js

---
+++ pages/api/delete_vendor.js
@@ -0,0 +1,53 @@
+// Fake users data
+import Cors from 'cors';
+import initMiddleware from '../../lib/init-middleware';
+import { prisma } from "./_base";
+const cors = initMiddleware(
+    Cors({
+      methods: ['GET', 'POST', 'OPTIONS'],
+    })
+)
+export default async function deleteVendor(req, res) {
+    await cors(req, res);
+    try {
+        switch (req.method) {
+            case 'POST':
+                return deleteVendor();
+            default:
+                return res.status(500).end(`Method ${req.method} Not Allowed`)
+        }
+        async function deleteVendor() {
+            const { vendor_id, user_id } = req.body;
+            if (vendor_id == "" || vendor_id == undefined)
+                return res.status(200).send({ status:'error', message: "Vendor id is mandatory" })
+            if (user_id == "" || user_id == undefined)
+                return res.status(200).send({ status:'error', message: "User id is mandatory" })
+            try {
+                const accountDet = await prisma.account__c.findUnique({
+                    where: {
+                        id: Number(user_id)
+                    }
+                });
+                if (!accountDet) {
+                    return res.status(200).send({ status:'error',  message: "Invalid account details" })
+                } else {
+                    
+                        await prisma.vendor__c.delete({
+                            where:{
+                                id: Number(vendor_id)
+                            }
+                        });
+                       
+                    return res.status(200).send({ status:'success',  message: "Vendor deleted successfully"})
+                }
+            } catch (e) {
+                res.status(200).send({ status:'error', message: e.message ? e.message : e });
+                return;
+            }// update otp process
+        }
+    } catch (error) {
+        res.status(200).send({ status:'error', message: error.message ? error.message : error })
+    } // get mobile number process
+
+}
+
