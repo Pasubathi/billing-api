c388e532e8c398bef23e1eee8b5540a90e16293d
murugan-pandurangan <p.murugan103@gmail.com>
2022-07-22 17:12:39 +0530

    Update delete_debit_note.js

--- pages/api/delete_debit_note.js
+++ pages/api/delete_debit_note.js
@@ -31,7 +31,7 @@ export default async function deleteDebit(req, res) {
                     return res.status(200).send({ status:'error',  message: "Invalid debit note id" })
                 } else {
 
-                    const debitOrder = await prisma.debit_order__c.findMany({
+                    const debitOrder = await prisma.debit_order__c.findUnique({
                         where: {
                             debit_note_id__c: Number(debit_id)
                         }
